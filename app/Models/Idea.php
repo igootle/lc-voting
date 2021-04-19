2ad1c29fe1f2d0fb3dbfcbcafb36c579981a5fbd
igoogle <igootle@gmail.com>
2021-04-19 16:16:32 +0700

    Episode 17 - Voting Database Structure

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -38,6 +38,11 @@ class Idea extends Model
        return $this->belongsTo(Status::class);
     }
 
+    public function votes()
+    {
+       return $this->belongsToMany(User::class, 'votes');
+    }
+
 
 
 }
